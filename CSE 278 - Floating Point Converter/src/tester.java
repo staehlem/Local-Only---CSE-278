bf2ee7934786588de85e12403cee69a90e5260b7
Erich Staehling <erich.staehling@gmail.com>
2015-02-23 22:47:25 -0500

    Version 0.1 - Given Starter Code
    Code given by prof. Stahr to start off with.

--- CSE 278 - Floating Point Converter/src/tester.java
+++ CSE 278 - Floating Point Converter/src/tester.java
@@ -1,9 +1,28 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class tester {
-
+	public static Scanner in;
+	public static PrintWriter out;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try { readAndConvert(); }
+		catch (Exception e) { System.out.println("Error: " + e.getMessage()); }
 	}
-
+	
+	private static void readAndConvert() throws Exception {
+		String dataIn = "", dataOut = "";
+		in = new Scanner(new File("nums.txt"));
+		out = new PrintWriter(new File("output.txt"));
+		
+		while(in.hasNextLine()) {
+			dataIn = in.nextLine();
+			// convert dataIn to dataOut
+			out.println(dataOut);
+		}
+		
+		in.close();
+		out.close();
+	}
 }
