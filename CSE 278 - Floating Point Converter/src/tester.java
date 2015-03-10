29b22d57100186dfca7d5ac27c2b76c08e5349cf
Erich Staehling <erich.staehling@gmail.com>
2015-03-09 18:33:01 -0700

    Version 0.5 (MILESTONE 1) - Major changes
    -fixed multiple bugs
    -updated output file name
    -changed class name
    -fixed spacing
    -improved formatting
    -extensive testing
    KNOWN BUGS/ERRORS: ~~~~~~~~~~~~
    -none known as of this update

--- CSE 278 - Floating Point Converter/src/tester.java
+++ CSE 278 - Floating Point Converter/src/tester.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class tester {
+public class Tester {
 	public static Scanner in;
 	public static PrintWriter out;
 	
@@ -18,7 +18,7 @@ public class tester {
 	private static void readAndConvert() throws Exception {
 		String dataIn = "", dataOut = "";
 		in = new Scanner(new File("nums.txt"));
-		out = new PrintWriter(new File("output.txt"));
+		out = new PrintWriter(new File("converted.txt"));
 		
 		// This loop handles the flow of the converter.  It determines where the read in string should go in terms of converter method.
 		while(in.hasNextLine()) {
@@ -54,33 +54,50 @@ public class tester {
 		
 		if(dataIn.charAt(0) == '-'){  // Determine the first binary # based on neg or pos value
 			ret += "1";
+			dataIn = dataIn.substring(1);  // Since first char is resolved, you can remove it 
 		} else {
 			ret += "0";
 		}
-		dataIn = dataIn.substring(1);  // Since first char is resolved, you can remove it 
+				
+		System.out.println();
 		
-		String[] leftAndRightOfTheDecimal = dataIn.split(".");  // The left and right hand side of the float are handled differently and therefore should be split and handled separately 
+		String[] leftAndRightOfTheDecimal = dataIn.split("\\.");  // The left and right hand side of the float are handled differently and therefore should be split and handled separately 
 		ret += normalConversionToBinary(leftAndRightOfTheDecimal[0]); // convert left of the decimal
 		ret += floatingPointConversionToBinary(leftAndRightOfTheDecimal[1]);  // convert right of the decimal
 		
 		return ret; // return converted string
 	}
 
+	/**
+	 * Conversion for the right side of the decimal
+	 * @param val
+	 * @return
+	 */
 	private static String floatingPointConversionToBinary(String val) {
+		val = "0." + val;	// This puts a zero and a decimal at the front of the integer that the split got rid of.  Makes the value of val a decimal #
 		String convertedValue = ".";  // the decimal was lost with the split from floatToBinary, added here
-		long remainder = 0;
+		float remainder = 0;
 		int i = 0;  // counter
 		
-		while(Integer.parseInt(val) > 0 && i < 24) {  // max of 23 digits in a 32 bit machine so i must be lower than 24
-			if(2 * Integer.parseInt(val) >= 1) {		// check remainder
-				remainder = (2 * Integer.parseInt(val))-1; // set remainder
+																																	//System.out.println("Made it to RIGHT side conversion " + val);
+																																	//System.out.println(Float.parseFloat(val));
+		
+		
+		while(Float.parseFloat(val) > 0 && i < 24) {  // max of 23 digits in a 32 bit machine so 'i' must be lower than 24
+			
+			if((Float.parseFloat(val) * 2) >= 1) {		// check remainder
+				remainder = ((2 * Float.parseFloat(val))-1); // set remainder
+																																	//System.out.println("what is the remainder right after i set it?? ----------> " + remainder);
 				convertedValue += "1";
 			} else {
-				remainder = (2 * Integer.parseInt(val)); // set remainder
+				remainder = (2 * Float.parseFloat(val)); // set remainder
+																																	//System.out.println("what is the remainder right after i set it in the else brach**?? ----------> " + remainder);
 				convertedValue += "0";
 			}
-			
+																																	//System.out.println(Float.parseFloat(val) + " ~~~~~~~~~~~~~~ after the else  ");
+																																	//System.out.println("what is the remainder right before val is updated?? ----------> " + remainder);
 			val = remainder + ""; //update val
+																																	//System.out.println(Float.parseFloat(val) + " ~~~~~~~~~~~~~~ after updateing val");
 			i++; // increment counter
 		}
 		
@@ -95,13 +112,13 @@ public class tester {
 	private static String normalConversionToBinary(String val) {
 		String convertedValue = "";
 		long remainder = 0;
-		
+																																	//System.out.println("Made it to LEFT side conversion " + val);
 		while(Integer.parseInt(val) > 0) {
 			remainder = (Integer.parseInt(val) % 2);	
 			val = Integer.parseInt(val)/2 + "";
 			convertedValue = remainder + convertedValue;
 		}
-		
+																																	//System.out.println(convertedValue);
 		return convertedValue;
 	}
 }	
